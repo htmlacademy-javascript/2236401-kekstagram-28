a713966e62eedf0c01bfde232e498ee1e0d52870
Яна Темиргалиева <kolesiko.yana@gmail.com>
2023-02-19 16:58:31 +0300

    creates functions for training

---
+++ js/functions.js
@@ -0,0 +1,56 @@
+// Функция для проверки длины строки.
+
+const checkLength = (string, number) => string.length <= number;
+
+checkLength('everything is perfect!', 23);
+
+// Функция для проверки, является ли строка палиндромом.
+
+const checkPolydrome = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  let newString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string.at(i);
+  }
+
+  return string === newString;
+};
+
+checkPolydrome('Лёша на полке клопа нашёл ');
+
+
+// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+const returnNumber = (string) => {
+  string = string.toString();
+  if (isNaN(parseFloat(string))) {
+    return NaN;
+  }
+  let newString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(parseFloat(string.at(i)))) {
+      newString += parseFloat(string.at(i));
+    }
+  }
+  return newString;
+};
+
+returnNumber('33 коровы и 2 кота');
+
+
+// Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
+
+const getString = (string, length, additionalСharacter) => {
+  let i = 0;
+  let newString = '';
+
+  while (length >= string.length + newString.length) {
+
+    newString += additionalСharacter.at(i);
+    i = i + 1 < additionalСharacter.length ? i + 1 : 0;
+  }
+  return newString + string;
+};
+
+getString('shine', 5, 'sun');
